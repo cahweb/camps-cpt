4b8335b6f4caa6b7a8e309c7daa8dd2318fb25c2
Alex Vecchi <alessandro.vecchi@ucf.edu>
2016-09-21 14:19:59 -0400

    commented debug code in camps.php

--- camps.php
+++ camps.php
@@ -16,9 +16,9 @@ $settings = array (
 
 /* Camps Custom Post Type ------------------- */
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 // Set form Enctype
 add_action('post_edit_form_tag', 'add_post_enctype');
